d7008ee416ed54fa685e4d50b7602acdfb093fbf
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-08-14 09:42:37 +0530

    Remove obsolete Django files and documentation

--- redactor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RedactorConfig(AppConfig):
-    name = 'redactor'
--- redactor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("preview/", views.preview, name="preview"),
-    path("download/", views.download, name="download"),
-    path("debug/", views.debug, name="debug"),
-]
